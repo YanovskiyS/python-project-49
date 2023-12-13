d2822d742c27e5b2b39c29bb8e33b887547b17a6
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2023-12-13 21:14:05 +0500

    fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,7 +7,8 @@ RULE = 'What number is missing in the progression?'
 def get_data_for_round():
     fist_element = randint(1, 10)
     step = randint(1, 10)
-    list_with_progression = list(range(fist_element, fist_element + step * 10, step))
+    last_element = fist_element + step * 10
+    list_with_progression = list(range(fist_element, last_element, step))
     hidden_index = randint(0, 8)
     number_for_answer = list_with_progression[hidden_index]
     list_with_progression[hidden_index] = '..'
